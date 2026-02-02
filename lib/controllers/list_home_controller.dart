aa10522f657784531414e400898e80524efc7b9f
candra kurnia <candra9508@gmail.com>
2026-02-02 16:21:57 +0700

    feat : implement ui list home

---
+++ lib/controllers/list_home_controller.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+class ListHomeController extends GetxController {
+  var loading = false.obs;
+
+  
+
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+}
