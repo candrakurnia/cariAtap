f75ff8939464dba15c9611fc6eab101019a4731e
candra kurnia <candra9508@gmail.com>
2026-02-04 16:08:00 +0700

    fix : remove exception text and message screen ui

--- lib/helper/dio_helper.dart
+++ lib/helper/dio_helper.dart
@@ -1,28 +1,27 @@
 import 'package:dio/dio.dart';
 
 class DioErrorHandler {
-  static Exception getErrorCode(DioException e) {
-    switch (e.error) {
-        case DioExceptionType.connectionTimeout:
-          return Exception('Connection timeout. Please try again later.');
-        case DioExceptionType.sendTimeout:
-          return Exception('Send timeout. Please try again.');
-        case DioExceptionType.receiveTimeout:
-          return Exception('Receive timeout. Please try again.');
-        case DioExceptionType.connectionError:
-          return Exception('No internet connection. Please check your network.');
-        case DioExceptionType.badResponse:
-          return Exception(
-            // e.response?.data.error?.message ??
-            'Unexpected error occurred. Please try again.',
-          );
-        case DioExceptionType.cancel:
-          return Exception('Request was cancelled.');
-        case DioExceptionType.unknown:
-        default:
-          return Exception('Unexpected error: ${e.message}');
+  static getErrorCode(DioException e) {
+    switch (e.type) {
+      case DioExceptionType.connectionTimeout:
+        return ('Connection timeout. Please try again later.');
+      case DioExceptionType.sendTimeout:
+        return ('Send timeout. Please try again.');
+      case DioExceptionType.receiveTimeout:
+        return ('Receive timeout. Please try again.');
+      case DioExceptionType.connectionError:
+        return ('No internet connection. Please check your network.');
+      case DioExceptionType.badResponse:
+        return (
+          // e.response?.data.error?.message ??
+          'Unexpected error occurred. Please try again.',
+        );
+      case DioExceptionType.cancel:
+        return ('Request was cancelled.');
+      case DioExceptionType.unknown:
+      default:
+        return ('Unexpected error: ${e.message}');
     }
-
   }
 
   static T handleWithParser<T>(
@@ -30,7 +29,7 @@ class DioErrorHandler {
     T Function(Map<String, dynamic>) fromJson,
   ) {
     if (e.type == DioExceptionType.badResponse &&
-            e.response?.statusCode == 401 ||
+            e.response?.statusCode == 400 ||
         e.response?.statusCode == 429 && e.response?.data != null) {
       return fromJson(e.response!.data);
     }
